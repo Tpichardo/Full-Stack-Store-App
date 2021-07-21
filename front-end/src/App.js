5d96a879e36661b4fe4c6523ed9a1c30cbe7f547
Talia Pichardo <taliapichardo@gmail.com>
2021-07-21 15:59:32 -0400

    Adds Index.js to App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,31 +1,23 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
-import { Switch, Router, Route } from "react-router-dom";
-import { apiURL } from "./util/apiURL.js";
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import Index from "./Pages/Index";
 import FourOFour from "./Pages/FourOFour.js";
 
-const API = apiURL();
 
 function App() {
-  const [items, setItems] = useState([]);
-  useEffect(() => {
-    axios
-      .get(`${API}/boutique`)
-      .then(
-        (response) => setItems(response.data),
-        (error) => console.log("get", error)
-      )
-      .catch((c) => console.warn("catch", c));
-  }, []);
   return (
     <div className="App">
-    <Router>
-      <Route path="*">
-        <FourOFour />
-      </Route>
-    </Router>
+      <Router>
+        <Switch>
+          <Route exact path="/boutique">
+            <Index />
+          </Route>
+          <Route path="*">
+            <FourOFour />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
