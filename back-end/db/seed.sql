d4eab64b612eaf07658670b9f91e13de2bc2cb5f
Talia Pichardo <taliapichardo@gmail.com>
2021-07-21 20:20:08 -0400

    Adds additional seeds

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,10 +1,21 @@
 \c boutique_dev;
 
 INSERT INTO boutique (name, brand, category, price, in_stock, url) VALUES
-('Marmont Bag', 'Gucci', 'handbag', 2200.00, true , 'https://www.pngitem.com/pimgs/m/347-3474254_gucci-marmont-leather-443497-dtdid-1000-hd-png.png'),
-('Supreme Ophidia', 'Gucci', 'handbag', 1490.00, true, 'https://cdn-images.farfetch-contents.com/12/56/25/66/12562566_11923829_480.jpg'),
-('Faux Leather Sweatpants', 'ALICE + OLIVIA', 'clothing', 295.00, true, 'https://static.orchardmile.us/webp/full/alice-and-olivia/bb94a71b314f5556beb1a488bce8cb4dc7b49aa2.webp'),
-('Ski 5 Wrap-Around Sunglassess', 'Christian Dior', 'sun glassess', 75.00, false, 'https://i.pinimg.com/originals/4e/1f/d3/4e1fd391305a1f3011c439f001af56c9.jpg' ),
-('Gold Plated Sunglasses AW 2000', 'Dior', 'sun glassess', 90.00, false, 'https://dtpmhvbsmffsz.cloudfront.net/posts/2017/07/03/595a0554291a35dd960a875c/m_595a055b78b31c74200a6d49.jpg'),
+('Marmont Bag', 'Gucci', 'Handbag', 2200.00, true , 'https://www.pngitem.com/pimgs/m/347-3474254_gucci-marmont-leather-443497-dtdid-1000-hd-png.png'),
+('Supreme Ophidia', 'Gucci', 'Handbag', 1490.00, true, 'https://cdn-images.farfetch-contents.com/12/56/25/66/12562566_11923829_480.jpg'),
+('Faux Leather Sweatpants', 'ALICE + OLIVIA', 'Pants', 295.00, true, 'https://static.orchardmile.us/webp/full/alice-and-olivia/bb94a71b314f5556beb1a488bce8cb4dc7b49aa2.webp'),
+('Ski 5 Wrap-Around Sunglassess', 'Christian Dior', 'Sunglassess', 75.00, false, 'https://i.pinimg.com/originals/4e/1f/d3/4e1fd391305a1f3011c439f001af56c9.jpg' ),
+('Airspeed Sunglasses', 'Christian Dior', 'Sunglassess', 200.00, false, 'https://assets.bigcartel.com/product_images/279228353/Dior+Airspeed+Sunglasses+main.jpg?auto=format&fit=max&h=1000&w=1000'),
 ('Hermes Clic', 'Hermes', 'handbag', 7550.00, true, 'https://assets.hermes.com/is/image/hermesproduct/hermes-clic-h-33-bag--077857CK89-front-1-300-0-850-850_b.jpg'),
-('Takashi Murakami', 'Louis Vuitton', 'handbag', 1395.00, false, 'https://cdn.shopify.com/s/files/1/1248/8217/products/803e3cd3c1410f61ee939257a7c88173_2000x.jpg?v=1605197755');
+('Takashi Murakami', 'Louis Vuitton', 'Handbag', 1395.00, false, 'https://cdn.shopify.com/s/files/1/1248/8217/products/803e3cd3c1410f61ee939257a7c88173_2000x.jpg?v=1605197755'),
+('CANDY Sunglassess', 'Juicy Couture', 'Sunglassess', 194.00, true, 'https://i1.go-optic.com/w900%7Ch600%7Cf/3jucyCANDYS.jpg'),
+('Milla Sunglasses', 'Juicy Couture', 'Sunglassess', 200.00, true, 'https://dtpmhvbsmffsz.cloudfront.net/posts/2013/04/21/5174b599bdb60028f3020a28/m_5174b59fbdb60028f3020a31.jpg'),
+('Pink Rhinestone sunglasses', 'Chanel', 'Sunglassess', 60.00, true, 'https://i.pinimg.com/originals/83/00/63/830063a10d1f872850b1fcda03bfc720.jpg'),
+('White Tank-Top', 'Von Dutch', 'Tops', 40.00, true, 'https://i.pinimg.com/originals/8d/e3/5f/8de35f342e5ecece188c7cdde9a9eb34.jpg'),
+('Pink Cropped T-Shirt', 'Von Dutch', 'Tops', 40.00, true, 'https://cdn.shopify.com/s/files/1/0257/8667/9341/products/VD_0032_1200x.jpg'),
+('Pink Tube Top', 'Baby Phat', 'Tops', 30.00, true , 'https://fashionista.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTY0NzM1MjEwMDk5ODQ1MDgz/baby-phat-forever-21-pink-velour-tube-top.jpg'),
+('Pink Juicy Couture Back Tracksuit Pants', 'Juicy Couture', 'Bottoms', 100, false, 'https://i.pinimg.com/originals/65/c4/50/65c4505ffd067ce974e930c6cba49010.jpg'),
+('White Tank-Top', 'Baby Phat', 'Tops', 30.00, false, 'https://fashionista.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTY0NzMzODgyNjg2NTE1MTU2/baby-phat-forever-21-white-tank.jpg'),
+('Black Asymmetical Crop Top', 'Baby Phat', 'Tops', 50.00, true, 'https://fashionista.com/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_620/MTY0NzMzODgyMTQ5ODQwODUy/baby-phat-forever-21-black-one-shouldered-crop-top.jpg'),
+('Artelier Limited Edition Jeans Culottes', 'Nicole Miller', 'Bottoms', 404.00, true, 'https://a.1stdibscdn.com/archivesE/upload/1121189/v_58693121549008043481/5869312_master.jpg?width=768'),
+('Retro Flare Denim Bellbottoms', 'Von Dutch', 'Bottoms', 300, false, 'https://s.abcnews.com/images/GMA/von-dutch-jeans-main-ht-jt-201026_1603724736274_hpMain_16x9_992.jpg');
