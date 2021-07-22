3e17298e6965fe0791d92c4c67f53cf9f8e56716
Sarah Weinstein <sbweins@gmail.com>
2021-07-22 11:37:13 -0400

    Added Routes for Show & Update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Index from "./Pages/Index";
 import FourOFour from "./Pages/FourOFour.js";
 import Home from "./Pages/Home.js";
+import UpdateItem from "./Components/UpdateItemForm";
+import Show from "./Pages/Show";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        {[]}
         <Switch>
           <Route exact path="/">
             <Home />
@@ -15,6 +18,15 @@ function App() {
           <Route exact path="/boutique">
             <Index />
           </Route>
+
+          <Route exact path="/boutique/:id">
+            <Show />
+          </Route>
+
+          <Route path="/boutique/:id/update">
+            <UpdateItem />
+          </Route>
+
           <Route path="*">
             <FourOFour />
           </Route>
