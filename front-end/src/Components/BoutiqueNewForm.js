af1b4129293d2df3377483932afd674a55b5f708
Talia Pichardo <taliapichardo@gmail.com>
2021-07-23 23:36:29 -0400

    Removes commented out code

--- front-end/src/Components/BoutiqueNewForm.js
+++ front-end/src/Components/BoutiqueNewForm.js
@@ -115,52 +115,6 @@ function BoutiqueNewForm() {
       </Link>
     </Container>
   );
-  // <div className="New">
-  //   <form onSubmit={handleSubmit}>
-  //     <label htmlFor="name">Name:</label>
-  //     <input
-  //       id="name"
-  //       value={item.name}
-  //       type="text"
-  //       onChange={handleTextChange}
-  //       placeholder="Name of item"
-  //     />
-  //     <label htmlFor="brand">Brand:</label>
-  //     <input
-  //       id="brand"
-  //       type="text"
-  //       value={item.brand}
-  //       placeholder="Brand of item"
-  //       onChange={handleTextChange}
-  //     />
-  //     <label htmlFor="category">Category:</label>
-  //     <input
-  //       id="category"
-  //       type="text"
-  //       value={item.category}
-  //       placeholder="Item category"
-  //       onChange={handleTextChange}
-  //     />
-  //     <label htmlFor="price">Price:</label>
-  //     <input
-  //       id="price"
-  //       type="number"
-  //       value={item.price}
-  //       placeholder="Item price"
-  //       onChange={handleTextChange}
-  //     />
-  //     <label htmlFor="in_stock">In Stock:</label>
-  //     <input
-  //       id="in_stock"
-  //       type="checkbox"
-  //       onChange={handleCheckboxChange}
-  //       checked={item.in_stock}
-  //     />
-  //     <br />
-  //     <input type="submit" />
-  //   </form>
-  // </div>
-  // );
 }
 
 export default withRouter(BoutiqueNewForm);
