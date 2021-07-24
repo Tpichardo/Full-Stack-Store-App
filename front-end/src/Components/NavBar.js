81c57f30aa5feeb7868a7c2ff096714bbaa94a3b
Talia Pichardo <taliapichardo@gmail.com>
2021-07-23 23:20:17 -0400

    Adds updates to navbar

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -4,30 +4,30 @@ import { Link } from 'react-router-dom'
 
 
 export default function NavBar() {
-  return (
-    <nav>
-    
-                <div className="burger"></div>
+    return (
+        <nav>
+
             <div className="burger"></div>
             <div className="burger"></div>
-      <h1>
-        <Link to="/">
-          <Button variant="outline-danger" size="lg">
-            Home
-          </Button>
-        </Link>{" "}
-        <Link to="/boutique">
-          <Button variant="outline-danger" size="lg">
-            What's Hot?
-          </Button>
-        </Link>{" "}
-        <Link to="/boutique/new">
-          <Button variant="outline-danger" size="lg">
-            Add Item!
-          </Button>
-        </Link>{" "}
-      </h1>
-    </nav>
-  );
+            <div className="burger"></div>
+            <h1>
+                <Link to="/">
+                    <Button variant="outline-danger" size="lg">
+                        Home
+                    </Button>
+                </Link>{" "}
+                <Link to="/boutique">
+                    <Button variant="outline-danger" size="lg">
+                        What's Hot?
+                    </Button>
+                </Link>{" "}
+                <Link to="/boutique/new">
+                    <Button variant="outline-danger" size="lg">
+                        That's Hot
+                    </Button>
+                </Link>{" "}
+            </h1>
+        </nav>
+    );
 }
 
