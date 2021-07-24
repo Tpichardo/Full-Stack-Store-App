899cff2cd357f7fe0069f5aae43cedd281e6392e
Talia Pichardo <taliapichardo@gmail.com>
2021-07-23 23:15:31 -0400

    Adds alt to home image

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,11 +1,11 @@
 import img from '../Images/gilwgsEDIT.png'
 
-export default function Home () {
+export default function Home() {
     return (
         <div className="Home">
             <h2>Welcome</h2>
             <h3>to the Boujee Betty Boutique</h3>
-            <img src={img}></img>
+            <img src={img} alt="Get in loser, we are going shopping"></img>
         </div>
-    ) 
+    )
 }
