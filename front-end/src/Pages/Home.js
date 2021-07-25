ee723ff17a773dd8900bc016b71389e11cf891a8
Talia Pichardo <taliapichardo@gmail.com>
2021-07-25 03:17:07 -0400

    Adds image component with img-fluid classname for image responsiveness

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,11 +1,12 @@
 import img from '../Images/gilwgsEDIT.png'
+import Image from "react-bootstrap/Image"
 
 export default function Home() {
     return (
         <div className="Home">
             <h2>Welcome</h2>
             <h3>to the Boujee Betty Boutique</h3>
-            <img className="bg" src={img} alt="Get in loser, we are going shopping" class="img-fluid" />
+            <Image className="img-fluid" src={img} alt="Get in loser, we are going shopping" />
         </div>
     )
 }
