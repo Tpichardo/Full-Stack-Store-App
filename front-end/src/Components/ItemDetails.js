6ba4cc7a1bbcfd710cd6c5b05edd0b919b361c64
Talia Pichardo <taliapichardo@gmail.com>
2021-07-25 01:30:05 -0400

    Adds edit and trash icon

--- front-end/src/Components/ItemDetails.js
+++ front-end/src/Components/ItemDetails.js
@@ -2,7 +2,10 @@ import { useEffect, useState } from "react";
 import { withRouter, Link } from "react-router-dom";
 import axios from "axios";
 import { apiURL } from "../util/apiURL";
+
 import { Container, Image, Row, Button } from "react-bootstrap";
+import { BsTrash } from 'react-icons/bs'
+import { FiEdit2 } from 'react-icons/fi';
 
 const API = apiURL();
 
@@ -40,10 +43,6 @@ function ItemDetails({ history, match }) {
     <div>
       <div className="showItemDetails">
         <h1>{item.name}</h1>
-        <h5>Brand: {item.brand}</h5>
-        <h5>Category: {item.category}</h5>
-        <h5>Price: ${item.price}</h5>
-        <h5>In stock: {JSON.stringify(item.in_stock)}</h5>
         <Container>
           <Row className="justify-content-md-center">
             <Image
@@ -54,6 +53,9 @@ function ItemDetails({ history, match }) {
               thumbnail
             />
           </Row>
+          <h5>Brand: {item.brand}</h5>
+          <h5>Price: ${item.price}</h5>
+          {JSON.stringify(item.in_stock) === "false" ? <h5 className="text-danger">Out of stock</h5> : <h5 className="text-success">In stock</h5>}
         </Container>{" "}
         <br></br>
         <div className="showItem"></div>
@@ -61,10 +63,12 @@ function ItemDetails({ history, match }) {
           <Button variant="outline-danger">Back</Button>
         </Link>{" "}
         <Link to={`/boutique/${item.id}/update`}>
-          <Button variant="outline-danger">Update</Button>
+          <Button variant="outline-danger">
+            Make Hotter <FiEdit2 />
+          </Button>
         </Link>{" "}
-        <Button variant="outline-danger" onClick={handleDelete}>
-          Delete
+        <Button variant="outline-danger">
+          Not Hot <BsTrash onClick={handleDelete} />
         </Button>{" "}
       </div>
     </div>
