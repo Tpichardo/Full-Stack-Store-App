ae45ef6ad823c907c187b3fb7a871789c37d8e9d
Talia Pichardo <taliapichardo@gmail.com>
2021-07-26 04:01:32 -0400

    Adds React-Bootstrap Navbar

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -1,32 +1,28 @@
-import Button from "react-bootstrap/Button";
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { Nav, Navbar, Container } from 'react-bootstrap';
 
 
 
 export default function NavBar() {
     return (
-        <nav>
-            <div className="burger"></div>
-            <div className="burger"></div>
-            <div className="burger"></div>
-            <h1>
-                <Link to="/">
-                    <Button variant="outline-danger" size="lg">
-                        Home
-                    </Button>
-                </Link>{" "}
-                <Link to="/boutique">
-                    <Button variant="outline-danger" size="lg">
-                        What's Hot?
-                    </Button>
-                </Link>{" "}
-                <Link to="/boutique/new">
-                    <Button variant="outline-danger" size="lg">
-                        That's Hot
-                    </Button>
-                </Link>{" "}
-            </h1>
-        </nav>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand>👚 Boujee Betty Boutique</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                    <Nav>
+                        <Nav.Item>
+                            <Nav.Link as={NavLink} to='/'>Home</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={NavLink} to='/boutique' >What's Hot</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={NavLink} to='/boutique/new'>That's Hot</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
-
